846b7bfd96708f1a246481f299f08693c4bba178
xarismy <xarismy@gmail.com>
2026-07-10 00:19:27 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "lab" {
+  name     = "rg-devops-lab-tf"
+  location = "swedencentral"
+}
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "vnet-tf-lab"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
