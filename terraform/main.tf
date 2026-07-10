6d13c75e17274376cdd4cc0754b6620314056d8a
xarismy <xarismy@gmail.com>
2026-07-10 17:05:08 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,4 +103,19 @@ resource "azurerm_network_security_rule" "web_https" {
   resource_group_name         = azurerm_resource_group.lab.name
   network_security_group_name = azurerm_network_security_group.web.name
 }
+resource "azurerm_network_security_rule" "app_8080" {
+  name                        = "allow-web-to-app"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
 
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+
+  source_address_prefix       = "10.10.1.0/24"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = azurerm_resource_group.lab.name
+  network_security_group_name = azurerm_network_security_group.app.name
+}
