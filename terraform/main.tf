031ec3021d8942a96228f7650c542bc0a2dd8dcd
xarismy <xarismy@gmail.com>
2026-07-10 16:53:12 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,3 +57,17 @@ resource "azurerm_network_security_group" "db" {
   location            = azurerm_resource_group.lab.location
   resource_group_name = azurerm_resource_group.lab.name
 }
+resource "azurerm_subnet_network_security_group_association" "web" {
+  subnet_id                 = azurerm_subnet.web.id
+  network_security_group_id = azurerm_network_security_group.web.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "app" {
+  subnet_id                 = azurerm_subnet.app.id
+  network_security_group_id = azurerm_network_security_group.app.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "db" {
+  subnet_id                 = azurerm_subnet.db.id
+  network_security_group_id = azurerm_network_security_group.db.id
+}
