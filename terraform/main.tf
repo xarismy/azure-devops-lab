dc62657cf1048f4e77eaaa8797d3c945c4e70664
xarismy <xarismy@gmail.com>
2026-07-15 12:18:38 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -135,3 +135,70 @@ resource "azurerm_network_security_rule" "db_5432" {
   resource_group_name         = azurerm_resource_group.lab.name
   network_security_group_name = azurerm_network_security_group.db.name
 }
+resource "azurerm_network_security_rule" "web_ssh" {
+  name                        = "allow-ssh"
+  priority                    = 120
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+
+  source_port_range          = "*"
+  destination_port_range     = "22"
+
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
+  resource_group_name         = azurerm_resource_group.lab.name
+  network_security_group_name = azurerm_network_security_group.web.name
+}
+resource "azurerm_public_ip" "web" {
+  name                = "pip-web-01"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+resource "azurerm_network_interface" "web" {
+  name                = "nic-web-01"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.web.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web.id
+  }
+}
+resource "azurerm_linux_virtual_machine" "web" {
+  name                = "vm-web-01"
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+
+  size           = "Standard_B2ats_v2"
+  admin_username = "xarismy"
+
+  network_interface_ids = [
+    azurerm_network_interface.web.id
+  ]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "xarismy"
+    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFy1aybC0UDBLhbdwphJ4B/WCkNa728PzSGspoD3z9Me xarismy@gmail.com"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
